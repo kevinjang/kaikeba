c0dcec10b71906e47968cdaaee009e6e1e384ddf
kevinjang <kevinjang@163.com>
2018-12-17 23:07:51 +0800

    database

---
+++ model/db.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql');
+
+const cfg = {
+    host: 'localhost',
+    user: 'root',
+    password: 'tingjiaN*&768*', // 连接池
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+}
+
+const pool = mysql.createPool(cfg);
+
+module.exports ={
+    query:function (sql,value) {
+        return new Promise((resolve,reject)=>{
+            //查询方式1，通过连接池获取连接进行查询
+            // pool.getConnection((err,conn)=>{
+            //     if(err) reject(err);
+            //     conn.query(sql,value,(err,results)=>{
+            //         if(err) reject(err);
+            //         else resolve(results);
+            //     });
+            //
+            //     conn.release();
+            //
+            // });
+            // console.log()
+            //查询方式2，直接通过连接池进行查询
+            //省略了创建连接、最终释放连接的操作
+            pool.query(sql,value,(err,results)=>{
+                if(err) reject(err);
+                else resolve(results);
+            });
+        });
+    },
+    pool
+}
--- routes/open-courses.js
+++ routes/open-courses.js
@@ -1,9 +1,21 @@
+
+
 var express = require('express');
 var router = express.Router();
-
+const {query} = require("../model/db");
+// const db = require('../model/db');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('open-courses', { title: 'open courses' });
+router.get('/',async (req, res, next)=> {
+    let sql = `select * from kkb.user where username=?`;
+
+    // db.sequelize.query(sql,'kkk').then((results)=>{
+    //     console.log(results.length);
+    // });
+
+    let users = await query(sql,['kkk']);
+    console.log(users[0].phone);
+    // next(users);
+    res.render('open-courses')
 });
 
 module.exports = router;
